bb44fbc9f1ef3b95c94f4b73b157bb4e617f5942
Roman Senderovich <roman.inet@gmail.com>
2016-03-26 20:56:04 +0300

    part 6 - transaction support changes

---
+++ src/main/java/com/roman/ws/actuator/health/GreetingHealthIndicator.java
@@ -0,0 +1,45 @@
+package com.roman.ws.actuator.health;
+
+
+import com.roman.ws.model.Greeting;
+import com.roman.ws.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+/**
+ * The GreetingHealthIndicator is a custom Spring Boot Actuator HealthIndicator
+ * implementation. HealthIndicator classes are invoked when the Actuator
+ * 'health' endpoint is invoked. Each HealthIndicator class assesses some
+ * portion of the application's health, returing a Health object which indicates
+ * that status and, optionally, additional health attributes.
+ *
+ */
+@Component
+public class GreetingHealthIndicator implements HealthIndicator {
+
+    /**
+     * The GreetingService business service.
+     */
+    @Autowired
+    private GreetingService greetingService;
+
+    @Override
+    public Health health() {
+
+        // Assess the application's Greeting health. If the application's
+        // Greeting components have data to service user requests, the Greeting
+        // component is considered 'healthy', otherwise it is not.
+
+        Collection<Greeting> greetings = greetingService.findAll();
+
+        if (greetings == null || greetings.size() == 0) {
+            return Health.down().withDetail("count", 0).build();
+        }
+
+        return Health.up().withDetail("count", greetings.size()).build();
+    }
+}
